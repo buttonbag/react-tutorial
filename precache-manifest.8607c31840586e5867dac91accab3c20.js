16563bd56495ff8bef2847d95126266e1ad788ed
Louie Camacho <louie.camacho@hogarth-ogilvy.com>
2020-03-20 13:19:42 -0500

    Updates

---
+++ precache-manifest.8607c31840586e5867dac91accab3c20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b42b9eaf765d14d70c130d499cf43cf5",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "a0e23d07983d32c4e714",
+    "url": "/react-tutorial/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "bde06dce91a38bb0c6a7",
+    "url": "/react-tutorial/static/js/2.80315e50.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-tutorial/static/js/2.80315e50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0e23d07983d32c4e714",
+    "url": "/react-tutorial/static/js/main.55ecf2e6.chunk.js"
+  },
+  {
+    "revision": "48186d3ac18516ac0690",
+    "url": "/react-tutorial/static/js/runtime-main.f060380d.js"
+  }
+]);
